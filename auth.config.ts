0d99f465ea7ce6a51fc191e0ce4d9f74d440702d
Peter Milanese <peterm@milanese.life>
2025-12-04 17:58:51 -0500

    Force Google account picker on login

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,11 @@ const authConfig: NextAuthConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: 'select_account',
+        },
+      },
     }),
   ],
   pages: {

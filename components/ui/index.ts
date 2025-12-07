846f74463225e1f83276cfb2aabead0030dea6c1
Peter Milanese <peterm@milanese.life>
2025-12-07 01:24:27 -0500

    feat: Add shadcn/ui component library
    Add components:
    - Dialog, DropdownMenu, Tabs, Tooltip
    - Select, Label, Textarea, Switch, Checkbox
    - Separator, Avatar, Skeleton
    
    Part of #74 UI/UX Overhaul Phase 4

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -16,6 +16,73 @@ export {
 
 export { Input } from './Input';
 
+export { Avatar, AvatarImage, AvatarFallback } from './avatar';
+
+export { Checkbox } from './checkbox';
+
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './dialog';
+
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from './dropdown-menu';
+
+export { Label } from './label';
+
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from './select';
+
+export { Separator } from './separator';
+
+export { Skeleton } from './skeleton';
+
+export { Switch } from './switch';
+
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
+
+export { Textarea } from './textarea';
+
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from './tooltip';
+
 // Custom components
 export { default as ButtonLink } from './ButtonLink';
 export type { ButtonLinkVariant, ButtonLinkSize } from './ButtonLink';
